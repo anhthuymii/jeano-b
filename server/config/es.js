e92c341ee1f3e66634734606fd97fe8269cb45f6
anhthuymii <dangthuy197real@gmail.com>
2023-12-07 12:43:19 +0700

    update id cloud

--- server/config/es.js
+++ server/config/es.js
@@ -38,10 +38,10 @@ const search = async (searchTerm) => {
   let config = {
     method: "post",
     maxBodyLength: Infinity,
-    url: "https://e8b77b4e16fb487ab7afb73a68b19224.us-central1.gcp.cloud.es.io/v1/_search",
+    url: "https://jeanostore.es.us-central1.gcp.cloud.es.io/v1/_search",
     headers: {
       Authorization:
-        "ApiKey alhXVTVvc0JFbWQ5ejNwTHVlZGE6c1RpODZIdTdSOXE4bzd0Zy1hSWhiZw==",
+        "ApiKey UEItV1Fvd0JXczdqV2pDZ056NGw6bG9YX2h4Qk1SbldOazZNSHF1cFZlQQ",
       "Content-Type": "application/json",
     },
     data: data,
